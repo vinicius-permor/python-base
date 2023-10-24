a4c987e1379169ae293aec29b2fa6a60852395ed
vinicius-permor <ma.vinicius.moraes@gmail.com>
2023-10-24 18:46:46 -0300

    sistema que lê varíaveis de ambiente(0.0.1)

---
+++ main.py
@@ -0,0 +1,18 @@
+_version_info = "0.0.1" 
+__autor__ = "Marcus Vinicius"
+__license__ = "Unlicense"
+
+
+import os
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, world"
+
+if current_language == 'pt_BR':
+    msg = "Olá mundo"
+elif current_language == 'it_IT':
+    msg = "Ciao, Mondo"
+elif current_language == 'fr_FR':
+    msg = "Bonjuor Monde"
+
+print(msg)
